b5929bf5b63798ef83b42ba93ae0e5e5313c139a
POOJA JAISWAL <63010780+jaispoojalpu@users.noreply.github.com>
2020-08-20 12:04:57 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+
+export const graphQLPath = '/graphql';
+export const graphiQLPath = '/graphiql';
+
+export const dev = process.env.NODE_ENV !== 'production';
+export const appPath = process.env.NODE_ENV === 'production' ? './build/app' : './src';
+
+export const HN_DB_URI = process.env.DB_URI || 'https://hacker-news.firebaseio.com';
+export const HN_API_VERSION = process.env.HN_API_VERSION || '/v0';
+export const HN_API_URL = process.env.HN_API_URL || `${HN_DB_URI}${HN_API_VERSION}`;
+
+export const HOST_NAME = process.env.HOST_NAME || 'localhost';
+export const APP_PORT = process.env.APP_PORT || 3000;
+export const HOST = (process.browser && window.location.host) || `${HOST_NAME}:${APP_PORT}`;
+
+export const APP_URI = `http://${HOST}`;
+export const GRAPHQL_URL = `${APP_URI}${graphQLPath}`;
+export const GRAPHIQL_URL = `${APP_URI}${graphiQLPath}`;
